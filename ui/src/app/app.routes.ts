b131711923e9eed701d87ae6ee954b43abf60b76
Artem Urvanov <temaptz@gmail.com>
2025-03-02 20:54:04 +0300

    dev

--- ui/src/app/app.routes.ts
+++ ui/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { MainComponent } from './pages/main/main.component';
-import { InstrumentComponent } from './pages/instrument/instrument.component';
-import { ListComponent } from './pages/list/list.component';
 
 export const routes: Routes = [
 
