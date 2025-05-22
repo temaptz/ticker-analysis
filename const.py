66622c8d7221aa6a9a4042f9f4c8d844507b4e86
Artem Urvanov <temaptz@gmail.com>
2025-05-22 12:14:14 +0300

    dev

--- const.py
+++ const.py
@@ -1,15 +1,16 @@
-TINKOFF_INVEST_TOKEN = ''
 DB_FILENAME = 'db.sqlite'
 DB_HOST = 'postgres'
 DB_NAME = 'main_db'
 DB_USERNAME = 'main_db_user'
 DB_PASSWORD = 'main_db_password'
+TINKOFF_INVEST_TOKEN = ''
 TELEGRAM_BOT_API_KEY = ''
 TELEGRAM_CHAT_IDS: list[int] = []
 Y_DISK_TOKEN = ''
 Y_API_KEY_ID = ''
 Y_API_SECRET = ''
 Y_API_FOLDER_ID = ''
+HUGGING_FACE_TOKEN = ''
 TICKER_LIST: list[str] = [
     'OZON',
     'SBER',
