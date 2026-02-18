e70491491fd6f455f317d9051bb5855488e5636c
Artem Urvanov <temaptz@gmail.com>
2026-02-18 18:18:46 +0300

    dev

--- lib/agent/tech.py
+++ lib/agent/tech.py
@@ -4,9 +4,8 @@ from lib.learn.ta_3_technical import TARGET_MAX_DAYS_COUNT
 
 def get_tech_buy_rate(instrument_uid: str):
     final_rate = 0
-    final_rate_percent = 0
+    final_rate_value = 0
     target_price_change = 0.1
-    target_days_distance = TARGET_MAX_DAYS_COUNT
     max_prediction = 0
     max_prediction_date = 0
     weeks_rate = []
@@ -16,7 +15,7 @@ def get_tech_buy_rate(instrument_uid: str):
     try:
         now = date_utils.get_day_prediction_time(date=datetime.datetime.now(tz=datetime.timezone.utc))
         date_from = now + datetime.timedelta(days=1)
-        date_to = now + datetime.timedelta(days=target_days_distance)
+        date_to = now + datetime.timedelta(days=TARGET_MAX_DAYS_COUNT)
 
         for day in date_utils.get_dates_interval_list(
                 date_from=date_from,
@@ -25,14 +24,13 @@ def get_tech_buy_rate(instrument_uid: str):
         ):
             day_rate = 0
             distance_days = (day - date_from).days
-            days_distance_multiply = 0 # Чем ближе день, тем выше оценка
-
-            if distance_days < TARGET_MAX_DAYS_COUNT:
-                days_distance_multiply = agent.utils.lerp(TARGET_MAX_DAYS_COUNT - distance_days, 0, TARGET_MAX_DAYS_COUNT, 0.75, 1)
-            elif distance_days < 90:
-                days_distance_multiply = agent.utils.lerp(90 - distance_days, 0, 70, 0.5, 0.75)
-            else:
-                days_distance_multiply = agent.utils.lerp(target_days_distance - distance_days, 0, target_days_distance - 90, 0, 0.5)
+            days_distance_multiply = agent.utils.lerp(
+                TARGET_MAX_DAYS_COUNT - distance_days,
+                0,
+                TARGET_MAX_DAYS_COUNT,
+                0.01,
+                1,
+            )
 
             if (pred := predictions.get_prediction(
                     instrument_uid=instrument_uid,
@@ -60,13 +58,13 @@ def get_tech_buy_rate(instrument_uid: str):
             if avg_rate > final_rate:
                 final_rate = avg_rate
 
-        final_rate_percent = 0 if is_no_predictions else int(max(0, min(final_rate, 1)) * 100)
+        final_rate_value = 0 if is_no_predictions else max(0, min(final_rate, 1))
 
     except Exception as e:
         logger.log_error(method_name='rsi_buy_rate', error=e, is_telegram_send=False)
 
     return {
-        'rate': final_rate_percent,
+        'rate': final_rate_value,
         'debug': {
             'max_prediction_date': max_prediction_date,
             'max_prediction_value': max_prediction,
@@ -76,16 +74,15 @@ def get_tech_buy_rate(instrument_uid: str):
 
 
 def get_tech_sell_rate(instrument_uid: str):
-    final_rate_percent = 0
-    target_days_distance = TARGET_MAX_DAYS_COUNT
-    days_before_positive_prediction = target_days_distance
+    final_rate_value = 0
+    days_before_positive_prediction = None
     predictions_list = []
     is_no_predictions = True
 
     try:
         now = date_utils.get_day_prediction_time(date=datetime.datetime.now(tz=datetime.timezone.utc))
         date_from = now + datetime.timedelta(days=1)
-        date_to = now + datetime.timedelta(days=target_days_distance)
+        date_to = now + datetime.timedelta(days=TARGET_MAX_DAYS_COUNT)
 
         for day in date_utils.get_dates_interval_list(
                 date_from=date_from,
@@ -105,17 +102,23 @@ def get_tech_sell_rate(instrument_uid: str):
 
                 if pred > 0.01:
                     delta_days = (day - date_from).days
-                    if delta_days < days_before_positive_prediction:
+                    if days_before_positive_prediction is None or delta_days < days_before_positive_prediction:
                         days_before_positive_prediction = delta_days
 
-        final_rate = agent.utils.linear_interpolation(days_before_positive_prediction, 0, target_days_distance, 0, 1)
-        final_rate_percent = 0 if is_no_predictions else int(max(0, min(final_rate, 1)) * 100)
+        final_rate = agent.utils.linear_interpolation(
+            days_before_positive_prediction,
+            0,
+            TARGET_MAX_DAYS_COUNT,
+            0,
+            1,
+        )
+        final_rate_value = 0 if is_no_predictions else max(0, min(final_rate, 1))
 
     except Exception as e:
         logger.log_error(method_name='price_sell_rate', error=e, is_telegram_send=False)
 
     return {
-        'rate': final_rate_percent,
+        'rate': final_rate_value,
         'debug': {
             'days_before_positive': days_before_positive_prediction,
             'predictions': predictions_list,
