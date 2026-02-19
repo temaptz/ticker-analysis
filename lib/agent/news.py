d758f44bca4dd6818d213eb70e3b37581e6ab4d2
Artem Urvanov <temaptz@gmail.com>
2026-02-19 10:52:29 +0300

    dev

--- lib/agent/news.py
+++ lib/agent/news.py
@@ -5,9 +5,12 @@ NEWS_CANDLES_COUNT = 7
 
 def get_news_buy_rate(instrument_uid: str):
     final_rate = 0
+    influence_score = None
 
     try:
-        if get_news_influence_score(instrument_uid=instrument_uid) > 0:
+        influence_score = get_news_influence_score(instrument_uid=instrument_uid)
+
+        if influence_score > 0:
             final_rate = 1
     except Exception as e:
         logger.log_error(method_name='get_news_buy_rate', error=e, is_telegram_send=False)
@@ -16,16 +19,22 @@ def get_news_buy_rate(instrument_uid: str):
         'rate': final_rate,
         'debug': {
             'rate': final_rate,
-            'news_count': 0,
+            'influence_score': influence_score,
+            'news_count': get_news_count(instrument_uid=instrument_uid),
+            'from_date': get_news_period()[0],
+            'to_date': get_news_period()[1],
         },
     }
 
 
 def get_news_sell_rate(instrument_uid: str):
     final_rate = 0
+    influence_score = None
 
     try:
-        if get_news_influence_score(instrument_uid=instrument_uid) < 0:
+        influence_score = get_news_influence_score(instrument_uid=instrument_uid)
+
+        if influence_score < 0:
             final_rate = 1
     except Exception as e:
         logger.log_error(method_name='get_news_sell_rate', error=e, is_telegram_send=False)
@@ -34,21 +43,45 @@ def get_news_sell_rate(instrument_uid: str):
         'rate': final_rate,
         'debug': {
             'rate': final_rate,
-            'news_count': 0,
+            'influence_score': influence_score,
+            'news_count': get_news_count(instrument_uid=instrument_uid),
+            'from_date': get_news_period()[0],
+            'to_date': get_news_period()[1],
         },
     }
 
 
 def get_news_influence_score(instrument_uid: str) -> float:
     try:
-        end_date = date_utils.get_day_prediction_time(date=datetime.datetime.now(datetime.timezone.utc))
-        start_date = end_date - datetime.timedelta(days=NEWS_CANDLES_COUNT)
+        period = get_news_period()
 
         return news.news.get_influence_score(
             instrument_uid=instrument_uid,
-            start_date=start_date,
-            end_date=end_date,
-        ) or 0
+            start_date=period[0],
+            end_date=period[1],
+        )
+
     except Exception as e:
         logger.log_error(method_name='get_news_influence_score', error=e, is_telegram_send=False)
     return 0
+
+def get_news_count(instrument_uid: str) -> int:
+    try:
+        period = get_news_period()
+        news_list = news.news.get_news_by_instrument_uid(
+            instrument_uid=instrument_uid,
+            start_date=period[0],
+            end_date=period[1],
+        )
+
+        return len(news_list)
+
+    except Exception as e:
+        logger.log_error(method_name='get_news_count', error=e, is_telegram_send=False)
+    return 0
+
+
+def get_news_period() -> (datetime.datetime, datetime.datetime):
+    end_date = date_utils.get_day_prediction_time(date=datetime.datetime.now(datetime.timezone.utc))
+    start_date = end_date - datetime.timedelta(days=14)
+    return start_date, end_date
