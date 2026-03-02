50911b62950ee081a0e7647b0fce7ffb841d1778
Artem Urvanov <temaptz@gmail.com>
2026-03-02 10:44:36 +0300

    dev

--- learn.py
+++ learn.py
@@ -5,9 +5,9 @@ print('IS DOCKER', docker.is_docker())
 print('IS PROD', docker.is_prod())
 
 if docker.is_prod():
-    telegram.send_message(f'[LEARN] Начало обучения {model.TA_3_fundamental}')
-    ta_3_fundamental_learn.generate_data()
-    telegram.send_message(f'[LEARN] Обучение {model.TA_3_fundamental} завершено')
+    # telegram.send_message(f'[LEARN] Начало обучения {model.TA_3_fundamental}')
+    # ta_3_fundamental_learn.generate_data()
+    # telegram.send_message(f'[LEARN] Обучение {model.TA_3_fundamental} завершено')
 
     telegram.send_message(f'[LEARN] Начало обучения {model.TA_3_volume}')
     ta_3_volume_learn.generate_data()
